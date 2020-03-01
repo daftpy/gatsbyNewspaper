5f9c40913bb97bab3b9088c7ed4bf4c0a9a2d487
Alexander Flood <daftpy@gmail.com>
2020-02-29 18:47:26 -0600

    Fixed image dimensions in ArticlePreview.

--- src/components/articlepreview.js
+++ src/components/articlepreview.js
@@ -6,7 +6,7 @@ export default (props) => {
   return (
     <article className="border-b-2 last:border-b-0 sm:flex py-6 items-center">
       {props.articleImage &&
-        <img className="sm:pr-4 rounded-sm sm:w-48 w-full flex-shrink-0" src="https://daftpy.com/wp-content/uploads/2020/02/white-house-129112.jpg" />
+        <img className="sm:pr-4 rounded-sm sm:w-48 sm:h-32 w-full flex-shrink-0" src={props.articleImage} />
       }
       <div className="py-4 sm:py-0">
         <div className="article-category font-bold text-sm text-red-600 tracking-wide uppercase">{props.articleCategory}</div>
